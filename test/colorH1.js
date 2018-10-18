8cd27cec246a5a6b646ebfd6368267d1c27b2de8
Lexa <lexa2987@mail.ru>
2018-10-18 14:24:24 +0300

    error

--- test/colorH1.js
+++ test/colorH1.js
@@ -1,6 +1,6 @@
 const colorH1 = (driver, By) => {
     try {
-        driver.findElement(By.css('h1')).getCssValue('color')
+        driver.findElement(By.css('input')).getCssValue('color')
             .then(color => {
                 if (color === 'rgba(255, 0, 0, 1)') {
                     console.log('ok');
