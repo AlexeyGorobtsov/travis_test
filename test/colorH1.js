ec0721dae465232889bf2a7540fe8b214c0a0b88
Lexa <lexa2987@mail.ru>
2018-10-18 15:59:15 +0300

    error

--- test/colorH1.js
+++ test/colorH1.js
@@ -1,26 +1,21 @@
-const error = require('./error');
-
 const colorH1 = (driver, By) => {
-    try {
-        driver.findElement(By.css('input')).getCssValue('color')
-            .then(color => {
-                if (color === 'rgba(255, 0, 0, 1)') {
-                    console.log('ok');
-                } else {
-                    // console.log(color);
-                }
-            }).catch(e => {
-                error.er();
-                console.error(`.catch(${e})`);
-                // const er = new Error('colorH1');
-                // er.name = 'My error';
-                // throw er;
 
-            });
-    } catch (e) {
-        console.error(`try/catch(${e})`);
+    driver.findElement(By.css('input')).getCssValue('color')
+        .then(color => {
+            if (color === 'rgba(255, 0, 0, 1)') {
+                console.log('ok');
+            } else {
+                // console.log(color);
+            }
+        }).catch(e => {
+            error.er();
+            console.error(`.catch(${e})`);
+            // const er = new Error('colorH1');
+            // er.name = 'My error';
+            // throw er;
 
-    }
+        });
+
 };
 
 module.exports = {
