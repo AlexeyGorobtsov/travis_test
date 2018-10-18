45381d53231f22c7859c5e1f468861d13e4b394f
Lexa <lexa2987@mail.ru>
2018-10-18 16:16:34 +0300

    error

--- test/colorH1.js
+++ test/colorH1.js
@@ -1,14 +1,24 @@
 const colorH1 = (driver, By) => {
+    try {
+        driver.findElement(By.css('input')).getCssValue('color')
+            .then(color => {
+                if (color === 'rgba(255, 0, 0, 1)') {
+                    console.log('ok');
+                } else {
+                    // console.log(color);
+                }
+            }).catch(e => {
+                eror
+                console.error(`.catch(${e})`);
+                // const er = new Error('colorH1');
+                // er.name = 'My error';
+                // throw er;
 
-    driver.findElement(By.css('input')).getCssValue('color')
-        .then(color => {
-            if (color === 'rgba(255, 0, 0, 1)') {
-                console.log('ok');
-            } else {
-                // console.log(color);
-            }
-        });
+            });
+    } catch (e) {
+        console.error(`try/catch(${e})`);
 
+    }
 };
 
 module.exports = {
